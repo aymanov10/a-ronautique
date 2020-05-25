ab31e83a7fe22c383b467fe45234d7b46b204b9b
Leumas73 <40551780+Leumas73@users.noreply.github.com>
2020-05-25 11:00:06 +0200

    Update settings.py

--- essai_aero/essai_aero/settings.py
+++ essai_aero/essai_aero/settings.py
@@ -78,15 +78,13 @@ WSGI_APPLICATION = 'essai_aero.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #{
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'essai_aero_1',
-        #'USER': 'postgres',
-        #'PASSWORD': 'gnonyaya123',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
+    {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'essai_aero_1',
+        'USER': 'postgres',
+        'PASSWORD': 'gnonyaya123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
